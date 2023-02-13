f64f764448dbe186af5f5d47e0691122cb76b805
DawidKolo <dawid_kolodziej@o2.pl>
2023-02-13 10:17:07 +0100

    DRY

--- main.py
+++ main.py
@@ -1,45 +1,24 @@
 import re
 
 
-def pubCount():
+def Count():
+    patternList = ['publications', 'documents', 'pages']
 # open log file
     s = open("data.txt", "r")
     data = s.read()
+    for i in patternList:
 
 # define pattern to search
-    pattern = r"(\d+) publications"
+        pattern = r"(\d+) "+ i
 
 # find all occurences of pattern
-    matches = re.findall(pattern,data)
-    newList = [eval(i) for i in matches]
+        matches = re.findall(pattern,data)
+        newList = [eval(i) for i in matches]
 
-    print("The number of the publications: " + str(sum(newList)))
+        print("The number of the "+ i + " " + str(sum(newList)))
 
-def docCount():
-    s = open("data.txt", "r")
-    data = s.read()
 
-    pattern = r"(\d+) documents"
-
-    matches = re.findall(pattern, data)
-    newList = [eval(i) for i in matches]
-
-    print("The number of the documents: " + str(sum(newList)))
-
-def pageCount():
-    s = open("data.txt", "r")
-    data = s.read()
-
-    pattern = r"(\d+) pages"
-
-    matches = re.findall(pattern, data)
-    newList = [eval(i) for i in matches]
-
-    print("The number of pages: "+str(sum(newList)))
-
-pubCount()
-docCount()
-pageCount()
+Count()
 
 
 
