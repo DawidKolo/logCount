8d927a75c6ede11138e3edc7820e115d08e7d73e
DawidKolo <dawid_kolodziej@o2.pl>
2023-02-13 12:36:03 +0100

    reformat

--- main.py
+++ main.py
@@ -3,23 +3,18 @@ import re
 
 def Count():
     patternList = ['publications', 'documents', 'pages']
-# open log file
+    # open log file
     s = open("data.txt", "r")
     data = s.read()
     for i in patternList:
+        # define pattern to search
+        pattern = r"(\d+) " + i
 
-# define pattern to search
-        pattern = r"(\d+) "+ i
-
-# find all occurences of pattern
-        matches = re.findall(pattern,data)
+        # find all occurences of pattern
+        matches = re.findall(pattern, data)
         newList = [eval(i) for i in matches]
 
-        print("The number of the "+ i + " " + str(sum(newList)))
+        print("The number of the " + i + " " + str(sum(newList)))
 
 
 Count()
-
-
-
-
